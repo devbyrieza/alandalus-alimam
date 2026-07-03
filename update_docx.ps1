468c6c66dc583943a73d22bbc5cf07a02710a422
devbyrieza <devbyrieza@gmail.com>
2026-07-03 15:47:04 +0700

    chore: Update Surat Pemberitahuan Kedatangan with clickable link and wording fixes

--- update_docx.ps1
+++ update_docx.ps1
@@ -7,9 +7,17 @@ $FindText = " yang telah dilegalisir"
 $ReplaceText = ""
 $selection.Find.Execute($FindText, $false, $false, $false, $false, $false, $true, 1, $false, $ReplaceText, 2)
 
-$FindText2 = "[LINK KONFIRMASI]"
-$ReplaceText2 = "Menu Welcome Day di Dashboard Pendaftar"
+$FindText2 = "Menu Welcome Day di Dashboard Pendaftar"
+$ReplaceText2 = "https://ppdb.pesantren-alimam.com/dashboard/pendaftar/welcome-day"
 $selection.Find.Execute($FindText2, $false, $false, $false, $false, $false, $true, 1, $false, $ReplaceText2, 2)
+
+$FindText3 = "Wali santri beserta santri baru"
+$ReplaceText3 = "calon santri baru"
+$selection.Find.Execute($FindText3, $false, $false, $false, $false, $false, $true, 1, $false, $ReplaceText3, 2)
+
+$FindText4 = "[LINK KONFIRMASI]"
+$ReplaceText4 = "https://ppdb.pesantren-alimam.com/dashboard/pendaftar/welcome-day"
+$selection.Find.Execute($FindText4, $false, $false, $false, $false, $false, $true, 1, $false, $ReplaceText4, 2)
 
 $doc.Save()
 $doc.ExportAsFixedFormat("C:\Users\itpua\Dev\Work\al-andalus\alandalus-alimam\public\documents\Surat_Pemberitahuan_Kedatangan.pdf", 17)
